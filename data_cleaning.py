fa6427fdc9d156e93149797929a5bf819236ecdf
Mane Minasyan <Mane_Minasyan@edu.aua.am>
2024-04-02 21:32:17 +0200

    Commiting changes

--- data_cleaning.py
+++ data_cleaning.py
@@ -5,5 +5,5 @@ df_orders = pd.read_csv("./data/orders.csv")
 df_products = pd.read_csv("./data/products.csv")
 df_regions = pd.read_csv("./data/regionss.csv")
 
-col_products = df_products.columns
-print(col_products)
+col_orders = df_orders.columns
+print(col_orders)
